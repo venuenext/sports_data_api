fc18ee21cbfef5f67643de6e4b5ba672167a5847
Jason Rylance <ecnalyr@gmail.com>
2016-01-22 11:47:05 -0600

    Adjusted formatting.

--- spec/lib/sports_data_api/nba/team_season_stats.rb
+++ spec/lib/sports_data_api/nba/team_season_stats.rb
@@ -5,12 +5,16 @@ describe SportsDataApi::Nba::TeamSeasonStats, vcr: {
     record: :new_episodes,
     match_requests_on: [:host, :path]
 } do
+
   before do
     SportsDataApi.set_key(:nba, api_key(:nba))
     SportsDataApi.set_access_level(:nba, 't')
   end
+
   let(:team_stats) { SportsDataApi::Nba.team_season_stats('583ed102-fb46-11e1-82cb-f4ce4684ea4c', 2014, 'reg') }
+
   subject { team_stats }
+
   describe 'meta methods' do
     it { should respond_to :season_id }
     it { should respond_to :season_year }
